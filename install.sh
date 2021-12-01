bb77dff4b9da8fa551ba99728067a5de5b1ebc1a
NeKosmic <76775569+NeKosmic@users.noreply.github.com>
2021-11-30 20:07:06 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,5 @@ apt-get install tesseract
 apt-get install nmap
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
-play audindex.mp3
-termimage datakawaii.png
 
 echo "no olvides tomar awita nwn "
