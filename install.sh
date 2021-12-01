700841269be462b2f988792a5b765bdc15fa5ec7
NeKosmic <76775569+NeKosmic@users.noreply.github.com>
2021-11-30 19:53:28 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/bash
 
+clear
 apt-get update
 apt-get upgrade
 apt-get install nodejs
 apt-get install libwebp
+apt-get install mc
 apt-get install ffmpeg
 apt-get install wget
 apt-get install tesseract
-apt-get install mc
 apt-get install nmap
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
-npm install github:adiwajshing/baileys
-npm audit fix --force
+play audindex.mp3
+termimage datakawaii.png
 
-echo "[*] Sexy quien lea estoUwU \n"Use npm start ó node index.js\n"para ejecutar al bot :3"
-echo "NeKosmic-Bot : Matt M."
+echo "no olvides tomar awita nwn "
